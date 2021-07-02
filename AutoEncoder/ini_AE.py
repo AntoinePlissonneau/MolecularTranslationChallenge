730ea90657519d625dfc58f8a123ebcb947b32de
Piotrowski Florian <79841404+Fpiotro@users.noreply.github.com>
2021-07-02 14:51:19 +0200

    Update ini_AE.py

--- AutoEncoder/ini_AE.py
+++ AutoEncoder/ini_AE.py
@@ -24,7 +24,7 @@ with open(INI_PATH.joinpath("preprocessing.ini"), 'w') as configfile:
 
 config = ConfigParser(allow_no_value=True)
 config['Data_parameters'] = {}
-config['Data_parameters']['data_name'] = 'AE_02052021'
+config['Data_parameters']['data_name'] = 'AE_2021'
 
 config['Model_parameters'] = {}
 config['Model_parameters']['n_channels'] = '1'
@@ -32,14 +32,14 @@ config['Model_parameters']['output_dim'] = '3'
 
 config['Training_parameters'] = {}
 config['Training_parameters']['start_epoch'] = '0'
-config['Training_parameters']['epochs'] = '1'
+config['Training_parameters']['epochs'] = '10'
 config['Training_parameters']['epochs_since_improvement'] = '0'
 config['Training_parameters']['model_lr'] = '5e-5'
 config['Training_parameters']['grad_clip'] = 'False'
 config['Training_parameters']['best_mse'] = '1000'
 config['Training_parameters']['print_freq'] = '1'
-config['Training_parameters']['checkpoint'] = 'True'
-config['Training_parameters']['checkpoint_path'] = 'F:/bms-molecular-translation/checkpoint_train/checkpoint_AE_14052021.pth (1).tar'
+config['Training_parameters']['checkpoint'] = 'False'
+config['Training_parameters']['checkpoint_path'] = './checkpoint/checkpoint_AE_2021.pth.tar'
 
 with open(INI_PATH.joinpath("params.ini"), 'w') as configfile:
     config.write(configfile)
